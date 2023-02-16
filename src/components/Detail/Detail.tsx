71b51832264c9dac15e8b336a4c737a46d8da80d
joaobru <joaopaulobruno123@gmail.com>
2023-02-16 00:04:43 -0300

    feat: create a component to use in mf

---
+++ src/components/Detail/Detail.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+ * @export
+ * @component
+ * @name Detail
+ *
+ * @description
+ * Componente de exemplo para exportar do mf.
+ */
+
+export const Detail = () => {
+  return (
+    <h2>Detail MF</h2>
+  )
+};
